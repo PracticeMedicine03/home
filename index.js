56ca8bd82709a3eaf8ba7ec3e768cbf9798a3d49
PracticeMedicine <102203580+PracticeMedicine03@users.noreply.github.com>
2023-11-21 11:24:20 +0800

    redirect shit and center shit at 404 page

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ function alertHelloWorld() {
 }
 
 function redirectDiscord() {
-    window.location.replace("https://discord.gg/WSyxgv7xf6");
+    window.location.assign("https://discord.gg/WSyxgv7xf6");
 }
 
 function redirectGitHub() {
-    window.location.replace("https://github.com/PracticeMedicine03");
+    window.location.assign("https://github.com/PracticeMedicine03");
 }
 
 function redirectYouTube() {
-    window.location.replace("https://youtube.com/@PracticeMedicine");
+    window.location.assign("https://youtube.com/@PracticeMedicine");
 }
 
 function redirectXTwitter() {
-    window.location.replace("https://twitter.com/practicemedalt");
+    window.location.assign("https://twitter.com/practicemedalt");
 }

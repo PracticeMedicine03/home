eb31ff9acff70ff0ed85d6c0023d311e66fb6409
PracticeMedicine <102203580+PracticeMedicine03@users.noreply.github.com>
2023-11-21 20:38:45 +0800

    ohohohoh

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ function alertHelloWorld() {
 }
 
 function redirectDiscord() {
-    window.location("https://discord.gg/WSyxgv7xf6");
+    window.location = "https://discord.gg/WSyxgv7xf6";
 }
 
 function redirectGitHub() {
-    window.location("https://github.com/PracticeMedicine03");
+    window.location = "https://github.com/PracticeMedicine03";
 }
 
 function redirectYouTube() {
-    window.location("https://youtube.com/@PracticeMedicine");
+    window.location = "https://youtube.com/@PracticeMedicine";
 }
 
 function redirectXTwitter() {
-    window.location("https://twitter.com/practicemedalt");
+    window.location = "https://twitter.com/practicemedalt";
 }

489972a4f1959f95f1aae16b0f8f1e9f2b5f2bdc
PracticeMedicine <102203580+PracticeMedicine03@users.noreply.github.com>
2024-01-20 20:26:00 +0800

    delete useless css and merge them into one file

--- mod_page.js
+++
@@ -1,7 +0,0 @@
-function downloadlatestPress() {
-    window.location.replace("https://practicemedicine03.github.io/practicemedicines-website/repo/pmu/downloads/pmu.zip");
-}
-
-function downloadbetaPress() {
-    window.location.replace("https://github.com/PracticeMedicine03/home/releases/download/v1.0.1106.2-pmu-beta/pmu.zip");
-}
---
+++ practicemedicine-main.js
@@ -0,0 +1,27 @@
+function alertHelloWorld() {
+    alert("Yes.");
+}
+
+function redirectDiscord() {
+    window.open("https://discord.gg/WSyxgv7xf6");
+}
+
+function redirectGitHub() {
+    window.open("https://github.com/PracticeMedicine03");
+}
+
+function redirectYouTube() {
+    window.open("https://youtube.com/@PracticeMedicine");
+}
+
+function redirectXTwitter() {
+    alert("my x/twitter acc got banned again for no reason.");
+}
+
+function downloadlatestPress() {
+    window.location.replace("https://practicemedicine03.github.io/practicemedicines-website/repo/pmu/downloads/pmu.zip");
+}
+
+function downloadbetaPress() {
+    window.location.replace("https://github.com/PracticeMedicine03/home/releases/download/v1.0.1106.2-pmu-beta/pmu.zip");
+}
